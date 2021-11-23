86cb49cb529fd8b957903cb867f03c5417db8bf1
peterwoodworth <woodwoop@amazon.com>
2021-11-22 17:52:48 -0800

    feat: add default area feature

--- src/issue.ts
+++ src/issue.ts
@@ -1,9 +1,12 @@
 import * as core from "@actions/core";
 import levenshtein from 'js-levenshtein';
 
-export interface IParameter {
+export interface IParameter extends IDefaultArea {
   area: string;
   keywords: string[];
+}
+
+export interface IDefaultArea {
   labels: string[];
   assignees: string[];
 }
@@ -12,6 +15,7 @@ export class Issue {
   private titleIssueWords?: string[];
   private bodyIssueWords?: string[];
   public parameters: IParameter[];
+  public defaultArea: IDefaultArea;
   private similarity: number;
   private bodyValue: number;
 
@@ -32,6 +36,7 @@ export class Issue {
       this.bodyIssueWords = body.split(/ |\(|\)|\./);
     }
     this.parameters = JSON.parse(core.getInput("parameters", {required: true}));
+    this.defaultArea = JSON.parse(core.getInput("default-area", {required: true}));
     this.similarity = +core.getInput("similarity", {required: false});
     this.bodyValue = +core.getInput("body-value", {required: false});
   }
@@ -63,28 +68,7 @@ export class Issue {
       
     console.log("Area scores: ", ...potentialAreas);
 
-    let winningArea = '';
-    let winners: Map<string,number> = new Map();
-    for (let area of potentialAreas.entries()) {
-      if(winners.size === 0) {
-        winners.set(area[0], area[1]);
-      } else if (area[1] > winners.values().next().value) {
-        winners = new Map();
-        winners.set(area[0], area[1]);
-      } else if (area[1] === winners.values().next().value) {
-        winners.set(area[0], area[1]);
-      }
-    }
-    // tiebreaker goes to the area with more *exact* keyword matches
-    if(winners.size > 1 && this.similarity !== 0) {
-      this.similarity = 0;
-      winningArea = this.determineArea();
-    } else if (winners.size > 0) {
-      winningArea = winners.keys().next().value;
-    } 
-      
-    winningArea = winners.keys().next().value;
-
+    const winningArea = this.decideWinner(potentialAreas);
     console.log("Winning area: " + winningArea);
       
     return winningArea;
@@ -118,6 +102,29 @@ export class Issue {
       })
     })    
     return potentialAreas;
+  }
+
+  private decideWinner(potentialAreas: Map<string, number>): string {
+    let winningArea = '';
+    let winners: Map<string,number> = new Map();
+    for (let area of potentialAreas.entries()) {
+      if(winners.size === 0) {
+        winners.set(area[0], area[1]);
+      } else if (area[1] > winners.values().next().value) {
+        winners = new Map();
+        winners.set(area[0], area[1]);
+      } else if (area[1] === winners.values().next().value) {
+        winners.set(area[0], area[1]);
+      }
+    }
+    // tiebreaker goes to the area with more *exact* keyword matches
+    if(winners.size > 1 && this.similarity !== 0) {
+      this.similarity = 0;
+      winningArea = this.determineArea();
+    } else if (winners.size > 0) {
+      winningArea = winners.keys().next().value;
+    } 
+    return winningArea;
   }
 
   private isSimilar(str1: string, str2: string): number {
